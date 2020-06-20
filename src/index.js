fddacf1c2f9f78132dab4536076369805be794dd
David Ko <david.holy.ko@gmail.com>
2020-06-19 22:56:37 -0400

    fix(hot-reloading): fix module hot undefined

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const Application = (
 );
 
 ReactDOM.render(Application, document.getElementById('root'));
-module.hot.accept();
+module.hot && module.hot.accept();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
