2a4eda490825af13241fe706bea8d63638e30daa
Simon Chen <46136260+simonchen92@users.noreply.github.com>
2020-06-23 14:01:17 -0400

    fix(webpack): added devtool to fix source mapping (#21)
    * fix(webpack): added devtool to fix source mapping
    
    * fix(webpack): change devtool option to load faster

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ module.exports = {
       patterns: [{ from: 'public', to: './' }],
     }),
   ],
+  devtool: 'eval-cheap-module-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'build'),
     open: true,
