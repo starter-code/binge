456d9fa8313746416fcc5b17bd9a564ef643ead9
Simon Chen <46136260+simonchen92@users.noreply.github.com>
2020-06-26 14:03:46 -0400

    feat(chart): added chart labels and fix display (#22)

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,12 +4,78 @@ import React from 'react';
 import { ChartSquare } from './ChartSquare';
 
 export const Chart = ({ data: chartInfo }) => {
+  const isVisible = chartInfo.length;
+
+  /**
+   * Generates y-axis season labels for given series
+   * @param {Array} data
+   * @returns {Array}
+   */
+  const generateSeasonLabels = (data) => {
+    let seasonLabels = [];
+    let seasonCount = 1;
+    _.each(data, (episodeData) => {
+      if (episodeData.episode === 1 && episodeData.season === seasonCount) {
+        const label = {
+          type: 'seasonLabels',
+          data: {
+            season: seasonCount,
+            episode: 0,
+          },
+        };
+        seasonLabels.push(label);
+        seasonCount++;
+      }
+    });
+    return seasonLabels;
+  };
+
+  /**
+   * Generates x axis labels for episode counts
+   * @param {Array} data
+   * @returns {Array}
+   */
+  const generateEpisodeNumberLabels = (data) => {
+    let episodeLabels = [];
+    let maxEpisodeCount = 0;
+    _.each(data, (episodeData) => {
+      if (episodeData.episode > maxEpisodeCount) {
+        const label = {
+          type: 'episodeLabels',
+          data: {
+            episode: episodeData.episode,
+            season: 999, // placeholder number so episode label numbers are always on the bottom
+          },
+        };
+        episodeLabels.push(label);
+        maxEpisodeCount++;
+      }
+    });
+    return episodeLabels;
+  };
+
+  const seasonLabels = generateSeasonLabels(chartInfo);
+  const episodeLabels = generateEpisodeNumberLabels(chartInfo);
+  const chartData = _.map(chartInfo, (data) => {
+    const output = {
+      type: 'episodeData',
+      data,
+    };
+    return output;
+  });
+
+  const newChartInfo = [...chartData, ...seasonLabels, ...episodeLabels];
+
   return (
     <div className="chart">
-      {_.map(chartInfo, (info, index) => {
-        const { data, type } = info;
-        return <ChartSquare key={index} type={type} data={data} />;
-      })}
+      {isVisible ? <div className="y-axis-label">Seasons</div> : null}
+      {isVisible ? <div className="x-axis-label">Episodes</div> : null}
+      <div className="chart-data">
+        {_.map(newChartInfo, (info, index) => {
+          const { data, type } = info;
+          return <ChartSquare key={index} type={type} data={data} />;
+        })}
+      </div>
     </div>
   );
 };
--- src/components/SeriesSearch/SeriesSearch.js
+++ src/components/SeriesSearch/SeriesSearch.js
@@ -17,70 +17,14 @@ const SeriesSearch = () => {
     setSearchValue('');
   };
 
-  /**
-   * Generates y-axis season labels for given series
-   * @param {Array} data
-   * @returns {Array}
-   */
-  const generateSeasonLabels = (data) => {
-    let seasonLabels = [];
-    let seasonCount = 1;
-    _.each(data, (episodeData) => {
-      if (episodeData.episode === 1 && episodeData.season === seasonCount) {
-        const label = {
-          type: 'seasonLabels',
-          data: {
-            season: seasonCount,
-            episode: 0,
-          },
-        };
-        seasonLabels.push(label);
-        seasonCount++;
-      }
-    });
-    return seasonLabels;
-  };
-
-  /**
-   * Generates x axis labels for episode counts
-   * @param {Array} data
-   * @returns {Array}
-   */
-  const generateEpisodeNumberLabels = (data) => {
-    let episodeLabels = [];
-    let maxEpisodeCount = 0;
-    _.each(data, (episodeData) => {
-      if (episodeData.episode > maxEpisodeCount) {
-        const label = {
-          type: 'episodeLabels',
-          data: {
-            episode: episodeData.episode,
-            season: 999, // placeholder number so episode label numbers are always on the bottom
-          },
-        };
-        episodeLabels.push(label);
-        maxEpisodeCount++;
-      }
-    });
-    return episodeLabels;
-  };
-
   const searchFunction = async (event) => {
     event.preventDefault();
     setChartInfo([]);
 
-    getEpisodes(searchValue).then(({ data: { results } }) => {
-      const seasonLabels = generateSeasonLabels(results);
-      const episodeLabels = generateEpisodeNumberLabels(results);
-      const chartData = _.map(results, (data) => {
-        const output = {
-          type: 'episodeData',
-          data,
-        };
-        return output;
-      });
-      setChartInfo([...chartData, ...seasonLabels, ...episodeLabels]);
-    });
+    const {
+      data: { results },
+    } = await getEpisodes(searchValue);
+    setChartInfo(results);
 
     resetInputValue();
   };
