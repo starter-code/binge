b12d6f3e8f60b0d6feee9a865303564a0e9663e2
David Ko <david.holy.ko@gmail.com>
2020-09-24 22:34:00 -0400

    fix(tv-poster): fix typo

--- src/components/Main/TvShowPoster.js
+++ src/components/Main/TvShowPoster.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const TVShowPoster = ({ data }) => {
   return (
     <Link to={`chart/${data.titleID}`}>
-      <img className="tv-show-poster-sm" src={data.imageURL}></img>;
+      <img className="tv-show-poster-sm" src={data.imageURL}></img>
     </Link>
   );
 };
